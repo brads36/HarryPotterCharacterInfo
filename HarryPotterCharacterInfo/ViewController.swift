aa3dafd3994c6fcf060e1b7f0861c3b82ca43080
Bryce Bradshaw <brycewbradshaw@icloud.com>
2020-04-29 14:55:42 -0600

    Initial Commit

---
+++ HarryPotterCharacterInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HarryPotterCharacterInfo
+//
+//  Created by Bryce Bradshaw on 4/29/20.
+//  Copyright © 2020 Bryce Bradshaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
